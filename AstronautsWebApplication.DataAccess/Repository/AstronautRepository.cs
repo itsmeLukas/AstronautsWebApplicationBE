b7164f0ec9d35524ee9b3723a24b4ebd3bcd1bd6
Lukáš Halouzka <lukas@jetveo.io>
2024-07-24 22:45:12 +0200

    Add Repository Pattern and UnitOfWork

---
+++ AstronautsWebApplication.DataAccess/Repository/AstronautRepository.cs
@@ -0,0 +1,25 @@
+﻿using AstronautsWebApplication.Data;
+using AstronautsWebApplication.DataAccess.Repository.IRepository;
+using AstronautsWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstronautsWebApplication.DataAccess.Repository
+{
+    internal class AstronautRepository : Repository<Astronaut>, IAstronautRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public AstronautRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Astronaut obj)
+        {
+            _db.Astronauts.Update(obj);
+        }
+    }
+}
---
+++ AstronautsWebApplication.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using AstronautsWebApplication.Data;
+using AstronautsWebApplication.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstronautsWebApplication.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IAstronautRepository Astronaut { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Astronaut = new AstronautRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
